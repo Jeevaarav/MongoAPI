5689ab1e9eecbab05551ec71acc2d33cd4660052
jeevaarav <2k19me070@kiot.ac.in>
2024-01-03 00:37:38 +0530

    nightupdate

--- app.js
+++ app.js
@@ -28,10 +28,32 @@ app.get('/Register',async(req,res)=>{
 app.post('/userRegister',async(req,res)=>{
     console.log(req.body);
     await mongoose.connection.collection("Register").insertOne(req.body).then((success)=>{
-        app.post("http://localhost:4200/Register")
+        
         // res.send("success");
     })
  }) 
+
+ app.post('/forgotuser',async(req,res)=>{
+    console.log(req.body);
+    await mongoose.connection.collection("Register").updateOne({regemail:req.body[0].mail},{$set:{regpass:req.body[1].regpass,regconfirm:req.body[1].regpass}}).then((updated)=>{
+            console.log("updated");
+    })
+ });
+
+ app.post('/updateservice',async(req,res)=>{
+    if(req.body.service.length){
+        console.log(req.body.service[0].email);
+        await mongoose.connection.collection("Register").updateOne({regemail:req.body.service[0].email},{$set:{service:req.body.service}}).then((updated)=>{
+                console.log("updated");
+        })
+    }
+    else{
+        console.log(req.body);
+        await mongoose.connection.collection("Register").updateOne({regemail:req.body.details.mail},{$set:{service:req.body.service.service}}).then((newupdate)=>{
+            console.log("new update");
+        })
+    }
+ })
 
 app.get('/vehicleBrands',async(req,res)=>{
     await mongoose.connection.collection("vehicleBrands").find().toArray().then((vehicle)=>{
